55d602cc91deae714350a9800f9e15daf352f2fc
Lex <totallabyss@gmail.com>
2022-04-29 12:04:47 +0700

    fix?

--- TestPath/Catalog.cs
+++ TestPath/Catalog.cs
@@ -1,149 +1,150 @@
 ﻿namespace TestPath
 {
-				public class Catalog
-				{
-								public string? path { get; set; }
+ public class Catalog
+ {
+  public string? path { get; set; }
 
-								public string? outFile { get; set; }
+  public string? outFile { get; set; }
 
-								public bool ShowAll { get; private set; }
+  public bool ShowAll { get; private set; }
 
-								private Flags flags;
-								private static int maxDepth { get; } = int.MaxValue;
+  private Flags flags;
+  private static int maxDepth { get; } = int.MaxValue;
 
-								public Catalog(string path)
-								{
-												this.path = path;
-												flags = new Flags();
-												outFile = outFile = Directory.GetCurrentDirectory() + "\\sizes " + DateTime.Today.ToString().Split(" ")[0] + ".txt";
-								}
+  public Catalog(string path)
+  {
+   this.path = path;
+   flags = new Flags();
+   outFile = outFile = Directory.GetCurrentDirectory() + "\\sizes " + DateTime.Today.ToString().Split(" ")[0] + ".txt";
+  }
 
-								private struct Flags
-								{
-												public bool quite, path, output, humanread;
-								}
-								public bool SetFlag(string str)
-								{
+  private struct Flags
+  {
+   public bool quite, path, output, humanread;
+  }
+  public bool SetFlag(string str)
+  {
 
-												if(str == "-q" || str == "--quite")
-																flags.quite = true;
-												else if(str == "-p" || str == "--path")
-																flags.path = true;
-												else if(str == "-o" || str == "--output")
-																flags.output = true;
-												else if(str == "-h" || str == "--humanread")
-																flags.humanread = true;
-												else
-																return false;
-												return true;
-								}
+   if(str == "-q" || str == "--quite")
+	flags.quite = true;
+   else if(str == "-p" || str == "--path")
+	flags.path = true;
+   else if(str == "-o" || str == "--output")
+	flags.output = true;
+   else if(str == "-h" || str == "--humanread")
+	flags.humanread = true;
+   else
+	return false;
+   return true;
+  }
 
-								public void PrintFlags()
-								{
-												Console.WriteLine("Quite = " + flags.quite + "\nPath = "
-																								+ flags.path + "\nOutput = " + flags.output
-																								+ "\nHumanread = " + flags.humanread);
-								}
+  public void PrintFlags()
+  {
+   Console.WriteLine("Quite = " + flags.quite + "\nPath = "
+												   + flags.path + "\nOutput = " + flags.output
+												   + "\nHumanread = " + flags.humanread);
+  }
 
-								public void RecOutput(string? path, int depth = 1)
-								{
-												if(depth >= maxDepth)
-																return;
+  public void RecOutput(string? path, int depth = 1)
+  {
+   if(depth >= maxDepth)
+	return;
 
-												if(string.IsNullOrWhiteSpace(path))
-																return;
+   if(string.IsNullOrWhiteSpace(path))
+	return;
 
-												DirectoryInfo dirInfo = new(path);
-												var fileSystemInfo = dirInfo.GetFileSystemInfos()
-																.Where(f => ShowAll || !f.Name.StartsWith("."))
-																.OrderBy(f => f.Name)
-																.ToList();
+   DirectoryInfo dirInfo = new(path);
+   var fileSystemInfo = dirInfo.GetFileSystemInfos()
+				   .Where(f => ShowAll || !f.Name.StartsWith("."))
+				   .OrderBy(f => f.Name)
+				   .ToList();
 
-												foreach(var fsItem in fileSystemInfo.Take(fileSystemInfo.Count))
-												{
-																PrefixPrint(depth, "--");
+   foreach(var fsItem in fileSystemInfo.Take(fileSystemInfo.Count))
+   {
+	PrefixPrint(depth, "--");
 
-																FileAttributes attr = File.GetAttributes(fsItem.FullName);
+	FileAttributes attr = File.GetAttributes(fsItem.FullName);
 
 
-																if(attr.HasFlag(FileAttributes.Directory))
-																{
-																				PrintPath(fsItem.Name, 1, GetDirectorySize(fsItem.FullName));
-																				RecOutput(fsItem.FullName, depth + 1);
-																}
-																else
-																{
-																				FileInfo file = new(fsItem.FullName);
-																				PrintPath(fsItem.Name, 1, (ulong)file.Length);
-																}
+	if(attr.HasFlag(FileAttributes.Directory))
+	{
+	 PrintPath(fsItem.Name, 1, GetDirectorySize(fsItem.FullName));
+	 RecOutput(fsItem.FullName, depth + 1);
+	}
+	else
+	{
+	 FileInfo file = new(fsItem.FullName);
+	 PrintPath(fsItem.Name, 1, (ulong)file.Length);
+	}
 
-												}
-												return;
-								}
+   }
+   return;
+  }
 
-								public void PrintPath(string name, int type, ulong sizeToPrint)
-								{
-												string finalPrint = name;
+  public void PrintPath(string name, int type, ulong sizeToPrint)
+  {
+   string finalPrint = name;
 
-												if(type == 0)
-																finalPrint = "-<" + name + ">";
+   if(type == 0)
+	finalPrint = "-<" + name + ">";
 
-												if(flags.humanread && sizeToPrint > 1024)
-																finalPrint += getHumanReadSize(sizeToPrint);
-												else
-																finalPrint += " (" + sizeToPrint + ")";
-												if(flags.quite || flags.quite && outFile != null)
-																File.AppendAllTextAsync(outFile, finalPrint + "\n");
-												else
-																Console.WriteLine(finalPrint);
-								}
+   if(flags.humanread && sizeToPrint > 1024)
+	finalPrint += getHumanReadSize(sizeToPrint);
+   else
+	finalPrint += " (" + sizeToPrint + ")";
+   if(flags.quite || flags.quite && outFile != null)
+	File.AppendAllTextAsync(outFile, finalPrint + "\n");
+   else
+	Console.WriteLine(finalPrint);
+  }
 
-								private string getHumanReadSize(ulong sizeToPrintInBytes)
-								{
-												string[] byteUnits = { " KB", " MB", " GB", " TB", " PB", " EB", " ZB", " YB" };
-												int i = 0;
+  private string getHumanReadSize(ulong sizeToPrintInBytes)
+  {
+   string[] byteUnits = { " KB", " MB", " GB", " TB", " PB", " EB", " ZB", " YB" };
+   int i = 0;
 
-												while(sizeToPrintInBytes > 1024)
-												{
-																sizeToPrintInBytes /= 1024;
-																i++;
-												}
+   while(sizeToPrintInBytes > 1024)
+   {
+	sizeToPrintInBytes /= 1024;
+	i++;
+   }
 
-												return " (" + Math.Round(Math.Max(sizeToPrintInBytes, 0.1), 2) + byteUnits[i] + ")";
-								}
+   return " (" + Math.Round(Math.Max(sizeToPrintInBytes, 0.1), 2) + byteUnits[i] + ")";
+  }
 
-								private void PrefixPrint(int depth, string v)
-								{
-												try
-												{
-																for(int i = 0; i < depth; i++)
-																{
-																				if(flags.quite && outFile != null)
-																								File.AppendAllTextAsync(outFile, v);
-																				else
-																								Console.Write(v);
-																}
-												}
-												catch(Exception e)
-												{
-																Console.WriteLine(e.ToString());
-												}
-								}
+  private void PrefixPrint(int depth, string v)
+  {
+   try
+   {
+	for(int i = 0; i < depth; i++)
+	{
+	 if(flags.quite && outFile != null)
+	  File.AppendAllTextAsync(outFile, v);
+	 else
+	  Console.Write(v);
+	}
+   }
+   catch(Exception e)
+   {
+	Console.WriteLine(e.ToString());
+   }
+  }
 
-								public ulong GetDirectorySize(string dir)
-								{
-												ulong startDirectorySize = 0;
-												if(!Directory.Exists(dir))
-																return startDirectorySize;
+  public ulong GetDirectorySize(string dir)
+  {
+   ulong startDirectorySize = 0;
+   if(!Directory.Exists(dir))
+	return startDirectorySize;
 
-												var currentDirectory = new DirectoryInfo(dir);
+   var currentDirectory = new DirectoryInfo(dir);
 
-												currentDirectory.GetFiles().ToList().ForEach(f => startDirectorySize += (ulong)f.Length);
+   currentDirectory.GetFiles().ToList().ForEach(f => startDirectorySize += (ulong)f.Length);
 
-												currentDirectory.GetDirectories().ToList()
-																.ForEach(d => startDirectorySize += GetDirectorySize(d.FullName));
+   currentDirectory.GetDirectories().ToList()
+				   .ForEach(d => startDirectorySize += GetDirectorySize(d.FullName));
 
-												return startDirectorySize;
-								}
-				}
+   return startDirectorySize;
+  }
+ }
 }
+//fixtab
